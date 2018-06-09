1eed2bacf335350441a3802f499853bb76260e14
Chris Al Gerges <chrisalgerges@hotmail.com>
2018-06-09 18:03:39 +0200

    added velocity publisher

--- sonar_detection/src/main.py
+++ sonar_detection/src/main.py
@@ -5,22 +5,35 @@ import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Range
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 def sonar_front(data):
     print(data.range)
+    if data.range < 0.0:
+        publisher_velocity.publish("linear: \
+        x: 0.0 \
+        y: 0.0 \
+        z: 0.0 \
+        angular: \
+        x:0.0 \
+        y:0.0 \
+        z:0.0")
+        publisher_speech.publish("Pardon me, could you please move aside?")
+    time.sleep(3)
 
-def sonar_back(data):
-    print(data.range)
+# def sonar_back(data):
+#     print(data.range)
 
-def laser_detector(data):
-    print(data.ranges)
-    time.sleep(1)
+# def laser_detector(data):
+#     print(data.ranges)
+#     time.sleep(1)
 
 
 if __name__ == '__main__':
     rospy.init_node("Chris_main")
-    publisher = rospy.Publisher("/speech", String, queue_size=10)
-    rospy.Subscriber("/pepper_robot/sonar/back", Range, sonar_back)
+    publisher_speech = rospy.Publisher("/speech", String, queue_size=10)
+    publisher_velocity = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    # rospy.Subscriber("/pepper_robot/sonar/back", Range, sonar_back)
     rospy.Subscriber("/pepper_robot/sonar/front", Range, sonar_front)
-    rospy.Subscriber("/pepper_robot/laser", LaserScan, laser_detector)
+    # rospy.Subscriber("/pepper_robot/laser", LaserScan, laser_detector)
     rospy.spin()
