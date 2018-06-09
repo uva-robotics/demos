0752a2c0df401ab4479afb219f1cbbce6b17f297
Chris Al Gerges <chrisalgerges@hotmail.com>
2018-06-09 16:50:30 +0200

    explored laser sensors

--- sonar_detection/src/main.py
+++ sonar_detection/src/main.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
 
 import rospy
-from std_msgs.msg import String
 import time
+from std_msgs.msg import String
 from sensor_msgs.msg import Range
+from sensor_msgs.msg import LaserScan
 
-# def sonar_front(data):
-#     print(data.range)
+def sonar_front(data):
+    print(data.range)
 
 def sonar_back(data):
     print(data.range)
+
+def laser_detector(data):
+    print(data.ranges)
+    time.sleep(1)
 
 
 if __name__ == '__main__':
     rospy.init_node("Chris_main")
     publisher = rospy.Publisher("/speech", String, queue_size=10)
-    # publisher.publish("Hello, i'm Pepper")
-
     rospy.Subscriber("/pepper_robot/sonar/back", Range, sonar_back)
-    # rospy.Subscriber("/pepper_robot/sonar/front", Range, sonar_front)
+    rospy.Subscriber("/pepper_robot/sonar/front", Range, sonar_front)
+    rospy.Subscriber("/pepper_robot/laser", LaserScan, laser_detector)
     rospy.spin()
